5ef78e12299ce8cc5bac2e99fb91b7c23c233cd2
Rajeev Kumar <rajeevjewar2@gmail.com>
2023-07-28 16:37:33 +0530

    Treasure Hunt project is completed.

---
+++ Day-03/project.py
@@ -0,0 +1,42 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+road_taken = input("You're at a cross road. Where do you want to go? Type 'left' or 'right' ")
+
+if road_taken == "left":
+    left_road = input("You come to a lack. There is an island in the middle of the lack. Type 'wait' to wait for a boat. Type 'swim' to swim across.")
+    if left_road == "wait":
+        choose_color = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which color do you choose?")
+        if choose_color == "red":
+            print("Burned by fire. Game Over.")
+        elif choose_color == "blue":
+            print("Eaten by beasts. Game Over.")
+        elif choose_color == "yellow":
+            print("You find treasure.You Win!")
+        else:
+            print("Game Over.")
+    else:
+        print("Attacked by trout. Game Over.")
+else:
+    print("Fall into a hole. Game Over.")
