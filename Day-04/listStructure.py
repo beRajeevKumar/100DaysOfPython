aab662f92a355dd06319fb25ea37aba338b85b13
Rajeev Kumar <rajeevjewar2@gmail.com>
2023-07-29 16:03:36 +0530

    list excercise is complete

--- Day-04/listStructure.py
+++ Day-04/listStructure.py
@@ -2,9 +2,37 @@
 # https://www.askpython.com/python/data-structures-in-python
 
 # Exercise 2 - Banker Roulette 
-import random
-# Split string method
-names_string = input("Give me everybody's names, separated by a comma. ")
-names = names_string.split(", ")
-print(f"{names[random.randint(0, len(names))]} is going to buy the meal today!")
-# ['Angela', 'Ben', 'Jenny', 'Michael', 'Chloe']
+# import random
+# # Split string method
+# names_string = input("Give me everybody's names, separated by a comma. ")
+# names = names_string.split(", ")
+# print(f"{names[random.randint(0, len(names))]} is going to buy the meal today!")
+# # ['Angela', 'Ben', 'Jenny', 'Michael', 'Chloe']
+
+
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+
+dirty_dozen = [fruits, vegetables]
+ 
+print(dirty_dozen[1][1])
+
+
+# 🚨 Don't change the code below 👇
+row1 = ["⬜️","️⬜️","️⬜️"]
+row2 = ["⬜️","⬜️","️⬜️"]
+row3 = ["⬜️️","⬜️️","⬜️️"]
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("Where do you want to put the treasure? ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this row 👇
+x=int(position[0])
+y=int(position[1])
+map[y-1][x-1]="X"
+
+#Write your code above this row 👆
+
+# 🚨 Don't change the code below 👇
+print(f"{row1}\n{row2}\n{row3}")
