6cacb4505648c4e8b7deb8073df69aee479b87e4
Rajeev Kumar <rajeevjewar2@gmail.com>
2023-07-31 19:31:57 +0530

    Maze Project is halfly sloved

---
+++ Day-06/maze_project.py
@@ -0,0 +1,17 @@
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+
+#  This code works only for some cased but there are some cases which are not working which are in Reeborg+World_Test folder. I will do that later.
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+        
+# while not at_goal():
+#     if right_is_clear():
+#         turn_right()
+#         move()
+#     elif front_is_clear():
+#         move()
+#     else:
+#         turn_left()
