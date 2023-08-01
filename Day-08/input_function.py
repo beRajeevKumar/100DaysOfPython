2f274d1d5cab7a319dd4c1636786d88c6070b480
Rajeev Kumar <rajeevjewar2@gmail.com>
2023-08-01 19:19:42 +0530

    Input functions are completed

---
+++ Day-08/input_function.py
@@ -0,0 +1,19 @@
+# Function with no input
+# def greet():
+#      print("Hello World!")
+#      print("Hello Coders!")
+#      print("Hello Programmers!")
+# greet()     
+
+# Function with 1 parameter.
+# def greet_with_name(name):
+#      print(f"Hello {name}")
+#      print(f"How do you do {name}")
+#      print(f"Hello Programmers, My name is {name}.")
+
+# greet_with_name("Rajeev Kumar")
+
+# Function with more than 1 parameter.
+def greet_with(name,location):
+    print(f"Hello Programmers, I a {name} and I live in {location}.")
+greet_with(location="Delhi",name="Rajeev")
