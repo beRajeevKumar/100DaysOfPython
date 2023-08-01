c40af15be2a09980109c9f18ef493ae1bd477283
Rajeev Kumar <rajeevjewar2@gmail.com>
2023-08-02 04:23:28 +0530

    Caesar Cipher Project is completed

--- Day-08/project_solution.py
+++ Day-08/project_solution.py
@@ -1,5 +1,8 @@
-import art
-print(art.logo)
+# import art
+# print(art.logo)
+
+from art import logo
+print(logo)
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
